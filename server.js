53aa414d78f8e2d63140607a2d2a3b2180ec89f0
aabid26 <abid.zeeshan26@gmail.com>
2025-08-04 23:14:52 +0530

    basic webapp using express

---
+++ server.js
@@ -0,0 +1,10 @@
+import express, { json } from "express";
+const PORT = process.env.PORT || 8080;
+const app = express();
+app.get("/",(req,res)=>{
+    res.json({message:"app is running on docker container"})
+});
+
+app.listen(PORT,()=>{
+    console.log(`app is running on ${PORT}`);
+});
